f00da89932680a16ae68fa1b208fff44d8e8c984
Chris Morris <morriswchris@gmail.com>
2021-12-04 16:53:02 -0500

    day2 part 1

---
+++ 2021/day2/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Measurements struct {
+	horizontal int
+	depth int
+}
+
+type Command struct {
+	direction string
+	amount int
+}
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	measurements := Measurements{horizontal: 0, depth: 0}
+	for scanner.Scan() {
+		command := commandDirections(scanner.Text())
+
+		switch command.direction {
+		case "forward":
+			measurements.horizontal += command.amount
+		case "down":
+			measurements.depth += command.amount
+		case "up":
+			measurements.depth -= command.amount
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	position := measurements.horizontal * measurements.depth
+	fmt.Printf("total position: %d",  position)
+}
+
+func commandDirections(command string) Command {
+	commandSplit := strings.Split(command, " ")
+	amount, _ := strconv.Atoi(commandSplit[1])
+	return Command{direction: commandSplit[0], amount: amount}
+}
